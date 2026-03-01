3c3532890899296906954b822bc5c46a715137d2
Efe Aşkın <kamilefeaskin@gmail.com>
2026-03-01 20:38:35 +0200

    Refactor calculator to advanced version with functions

--- Basic_Calculator.c
+++ Basic_Calculator.c
@@ -1,44 +1,91 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// Fonksiyon prototipleri
+double toplama(double a, double b);
+double cikarma(double a, double b);
+double carpma(double a, double b);
+double bolme(double a, double b);
+void menu_goster();
 
 int main() {
-    int num1, num2, k = 1;
+    double num1, num2;
     char op;
+    int devam = 1;
 
-    while (k != 0) {
+    printf("=== Advanced C Calculator ===\n");
+
+    while (devam) {
+
+        menu_goster();
+
         printf("Enter first number: ");
-        scanf("%d", &num1);
-
-        printf("Enter second number: ");
-        scanf("%d", &num2);
+        while (scanf("%lf", &num1) != 1) {
+            printf("Invalid input. Please enter a number: ");
+            while (getchar() != '\n');
+        }
 
         printf("Enter operator (+, -, *, /): ");
-        scanf(" %c", &op); 
+        scanf(" %c", &op);
+
+        printf("Enter second number: ");
+        while (scanf("%lf", &num2) != 1) {
+            printf("Invalid input. Please enter a number: ");
+            while (getchar() != '\n');
+        }
 
         switch (op) {
             case '+':
-                printf("Result: %d + %d = %d\n", num1, num2, num1 + num2);
+                printf("Result: %.2lf\n", toplama(num1, num2));
                 break;
             case '-':
-                printf("Result: %d - %d = %d\n", num1, num2, num1 - num2);
+                printf("Result: %.2lf\n", cikarma(num1, num2));
                 break;
             case '*':
-                printf("Result: %d * %d = %d\n", num1, num2, num1 * num2);
+                printf("Result: %.2lf\n", carpma(num1, num2));
                 break;
             case '/':
-                if (num2 != 0) {
-                    printf("Result: %d / %d = %.2f\n", num1, num2, (float)num1 / num2);
-                } else {
-                    printf("Error: DIVISION BY ZERO!!\n");
-                }
+                if (num2 != 0)
+                    printf("Result: %.2lf\n", bolme(num1, num2));
+                else
+                    printf("Error: Division by zero!\n");
                 break;
             default:
-                printf("Error: Invalid operator\n");
+                printf("Invalid operator!\n");
         }
-        
-        printf("Calculator: Press 0 to exit, any other number to continue: ");
-        scanf("%d", &k);
+
+        printf("\nPress 1 to continue, 0 to exit: ");
+        scanf("%d", &devam);
+        printf("\n");
     }
 
+    printf("Calculator closed. Goodbye!\n");
     return EXIT_SUCCESS;
+}
+
+// Fonksiyon tanımları
+
+double toplama(double a, double b) {
+    return a + b;
+}
+
+double cikarma(double a, double b) {
+    return a - b;
+}
+
+double carpma(double a, double b) {
+    return a * b;
+}
+
+double bolme(double a, double b) {
+    return a / b;
+}
+
+void menu_goster() {
+    printf("\nSelect an operation:\n");
+    printf("+  Addition\n");
+    printf("-  Subtraction\n");
+    printf("*  Multiplication\n");
+    printf("/  Division\n\n");
 }
